8ab2b3939b7a18b97bc9d4b355edc7c631917655
Somya Gupta <guptasomya118@gmail.com>
2025-03-31 19:47:40 +0530

    add for ELK

--- src/main/java/com/training/NotificationService/service/NotificationService.java
+++ src/main/java/com/training/NotificationService/service/NotificationService.java
@@ -3,35 +3,23 @@ import com.training.NotificationService.model.Notification;
 //import com.training.OrderService.event.OrderEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class NotificationService {
+    Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
 
     @KafkaListener(topics = "orderTopic", groupId = "notificationGroup")
     public void handleOrderPlacedNotification(ConsumerRecord<String, String> record) {
-        System.out.println("Received Notification: "+ record.key()+" "+record.value());
+        LOGGER.info("Received Notification: {} {}",record.key(),record.value());
     }
 
     @KafkaListener(topics = "paymentTopic", groupId = "notificationGroup")
     public void handlePaymentSuccessNotification(ConsumerRecord<String, String> record) {
-        System.out.println("Received Notification: "+ record.key()+" "+record.value());
+        LOGGER.info("Notification Received: {} {}",record.key(),record.value());
     }
-    /*
-    @KafkaListener(topics = "orderplacedTopic", groupId = "notificationGroup")
-    public void handleNotification(ConsumerRecord<String, String> record) {
-        //log.info("Received Notification: Order ID: {}, Customer ID: {}, Message: {}",
-          //      orderEvent.getOrderId(), orderEvent.getCustomerId(), orderEvent.getMessage());
-        System.out.println("Received Notification: Order ID: "+ record.key()+"Message: " +record.value());
-    }*/
-
-    /*@KafkaListener(topics = "orderTopic", groupId = "orderGroup")
-    public void listenOrder(ConsumerRecord<String, String> record) {
-        log.info("📩 Received Order Notification: Key: {}, Value: {}",
-                record.key(), record.value());
-        System.out.println(" Received Order Notification: Key: "+record.key()+
-                        ", Value: "+record.value());
-    }*/
 }
